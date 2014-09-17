52142a0f36018e2f9efb0a5e491ee5e0f56fbaa6
lesterzone <lester.angulo@dotcreek.com>
2014-09-17 11:52:56 -0600

    Fixes for device and display first room event
    Added util function to check if device is android
    Fixed display first event of room
    Sorted events on rooms/:id
    Inline documentation

--- App/app/scripts/models/room.js
+++ App/app/scripts/models/room.js
@@ -1,3 +1,10 @@
+/**
+ * @class Room
+ * @constructor
+ * @name App.Model.Room
+ * @property {String} urlRoot API end point for this model
+ * @property {Object} defaults Default properties for this model
+ */
 (function() {
     'use strict';
 
@@ -7,15 +14,55 @@
 
         defaults: {},
 
+        /**
+         * @name Room#parse
+         * @memberOf Room
+         * @method parse
+         * @description Set properties and modify JSON response from api
+         * @param  {Object} data
+         */
         parse: function(data) {
+            /**
+             * Set a property currentEvent to a model
+             * @type {Object}
+             */
             this.currentEvent = data.currentEvent;
+
+            /**
+             * If currentEvent is empty, set it to false, will help on template
+             * validations
+             */
             if (_.isEmpty(data.currentEvent)) {
                 this.currentEvent = false;
             }
-            this.upcoming = data.upcoming || [];
-            this.upcomingFirst = data.upcoming ? data.upcoming[0] : false;
-            if (this.upcomingFirst) {delete data.upcoming[0];}
 
+            /**
+             * Set a property upcoming
+             * @type {Object}
+             */
+            this.upcoming = data.upcoming || [];
+
+            if (!data.upcoming.length) {
+                return data;
+            }
+
+            data.upcoming = data.upcoming.sort(function(a, b) {
+                /**
+                 * Convert string date value to date and do the substraction
+                 */
+                return new Date(a.startAt) - new Date(b.startAt);
+            });
+
+            /**
+             * Set property upcomingFirst
+             * @type {Object}
+             */
+            this.upcomingFirst = data.upcoming[0] || false;
+
+            /**
+             * Return an object based on API response after set all properties
+             * to current model
+             */
             return data;
         }
     });
